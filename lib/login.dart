0ceda4aad55e6ba095683e433b364a071099b4cc
kafwihi <kafwihi@gmail.com>
2019-11-04 10:49:09 +0300

    initial

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFFFB415B),
+
+      ),
+      home: LoginPage(),
+    );
+  }
+}
 
 class LoginPage extends StatefulWidget{
   @override
